f4b9d611d5ee5babcb679ec23d2162bcd96c296d
[sliperly] <[sliperly@gmail.com]>
2021-04-26 06:18:32 +0400

    New

--- 01_Поиск_среднего.py
+++ 01_Поиск_среднего.py
@@ -1,8 +1,8 @@
-
-item = input ("Ввести число ")
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+item = input("Ввести число ")
 low = 0
-high = len (list) - 1
-mid = (low + high) / 2
-guess = list [mid]
-if guess < item:
+high = len(my_list) - 1
+mid = int((low + high) / 2)
+guess = my_list[mid]
+if guess < int(item):
     low = mid + 1
