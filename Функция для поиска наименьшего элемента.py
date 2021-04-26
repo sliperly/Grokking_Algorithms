dcdaf754aa0d580d720a5b677621f48fe35df48e
[sliperly] <[sliperly@gmail.com]>
2021-04-26 05:39:30 +0400

    New

---
+++ Функция для поиска наименьшего элемента.py
@@ -0,0 +1,8 @@
+def find_smallest(arr):
+    smallest = [0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
